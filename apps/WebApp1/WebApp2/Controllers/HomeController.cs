847e7832084911de17d05939fd4cbb5b52856ca5
Alexandro Lopes <alexanlp@gmail.com>
2021-12-20 22:12:49 -0300

    Teste com elementos e RESUMÃO

---
+++ apps/WebApp1/WebApp2/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestSharp;
+using System.Diagnostics;
+using WebApp2.Models;
+
+namespace WebApp2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            var client = new RestClient(new Uri(_configuration.GetSection("url").Value));
+            var request = new RestRequest("weatherforecast", Method.GET, DataFormat.Json);
+
+            var result = client.Execute<List<WeatherForecast>>(request);
+            if (result != null)
+            {
+                return View(result.Data.First());     
+            }
+            
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
